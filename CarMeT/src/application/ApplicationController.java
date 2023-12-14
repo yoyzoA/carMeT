8b74704eb8f601c92a8b6a1694b0a32fe41fb9d2
eliejoefarah <eliejoefarah@gmail.com>
2023-12-14 10:22:37 +0200

    commit

--- CarMeT/src/application/ApplicationController.java
+++ CarMeT/src/application/ApplicationController.java
@@ -18,8 +18,8 @@ public class ApplicationController {
 	private Scene scene;
 	private Parent root;
 
-	//values of the signup page
-	@FXML	
+	// values of the signup page
+	@FXML
 	TextField usernameSignup_textfield;
 	@FXML
 	TextField phonenumberSignup_textfield;
@@ -34,142 +34,152 @@ public class ApplicationController {
 	TextField email_textfield;
 	@FXML
 	PasswordField password_passwordfield;
- 
+
 	public void SignUpContinue(ActionEvent event) throws IOException {
 		try {
-            String username = usernameSignup_textfield.getText();
-            String email = emailSignup_textfield.getText();
-            String password = passwordSignup_passwordfield.getText();
-            String phoneNumber = phonenumberSignup_textfield.getText();
-            Thread.sleep(1000);
-            User user = new User(username,email,password,phoneNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }{}
+			String username = usernameSignup_textfield.getText();
+			String email = emailSignup_textfield.getText();
+			String password = passwordSignup_passwordfield.getText();
+			String phoneNumber = phonenumberSignup_textfield.getText();
+			Thread.sleep(1000);
+			User user = new User(username, email, password, phoneNumber);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		{
+		}
 		root = FXMLLoader.load(getClass().getResource("SignUpContinuePage.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		String css = this.getClass().getResource("application.css").toExternalForm();
 		scene.getStylesheets().add(css);
 		stage.setScene(scene);
 		stage.show();
 	}
- 
- public void Home(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	 }
 
-	  public void HomeSignedIn(ActionEvent event) throws IOException {
-		while(true){
-			try {
-			User user = new User();
-			
-            String username = username_textfield.getText();
-            String email = email_textfield.getText();
-            String password = password_passwordfield.getText();
-			user.userSignIn(username,password,email);
-            Thread.sleep(1000);
-			if(user.userID==0){
-				return;
-			}
-            break;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }{}}
+	public void Home(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		String css = this.getClass().getResource("application.css").toExternalForm();
 		stage.setScene(scene);
 		stage.show();
-	 }
- 
- public void Explore(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("ExplorePage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  scene.getStylesheets().add(css);
-	  stage.setScene(scene);
-	  stage.show();
-	 }
- public void Profile(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-
-	 }
- public void MyPurchases(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("MyPurchasesPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	 }
- 
- public void SellPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("SellPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	  
-	 }
- public void SellCarPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("SellCarPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	  
-	 }
- public void SellPartPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("SellPartPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	  
-	 }
- 
- public void LogInPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	 }
- 
- public void SignUpPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  scene.getStylesheets().add(css);
-	  stage.setScene(scene);
-	  stage.show();
-	 }
- 
- public void BuyPage(ActionEvent event) throws IOException {
-	  Parent root = FXMLLoader.load(getClass().getResource("BuyPage.fxml"));
-	  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  scene = new Scene(root);
-	  String css = this.getClass().getResource("application.css").toExternalForm();
-	  stage.setScene(scene);
-	  stage.show();
-	 }
 	}
 
+	public void HomeSignedIn(ActionEvent event) throws IOException {
+		while (true) {
+			try {
+				User user = new User();
+
+				String username = username_textfield.getText();
+				String email = email_textfield.getText();
+				String password = password_passwordfield.getText();
+				user.userSignIn(username, password, email);
+				Thread.sleep(1000);
+				if (user.userID == 0) {
+					return;
+				}
+				break;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			{
+			}
+		}
+		Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void Explore(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ExplorePage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	public void Profile(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	public void MyPurchases(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("MyPurchasesPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void SellPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("SellPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	public void SellCarPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("SellCarPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	public void SellPartPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("SellPartPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	public void LogInPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void SignUpPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void BuyPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("BuyPage.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		stage.setScene(scene);
+		stage.show();
+	}
+}
